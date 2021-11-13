c45c4fb4713f3d9f6ede81d64ca6ff6d2d0e7248
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-11-12 21:59:24 -0500

    added the code

---
+++ hw-15.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+char **parse_args(char *line){
+    int i = 0;
+    int totalDelim = 1;
+    while(line[i]){
+        if (line[i] == ' '){
+            totalDelim++;
+        }
+        i++;
+    }
+    char **result = malloc(totalDelim * sizeof(char *));
+    char *current;
+    i = 0;
+    while (current = strsep(&line, " \n"))
+    {
+        if (*current){
+            result[i] = current;
+            i++;
+        }
+    }
+    result[i] = NULL;
+    return result;
+}
+
+int main(){
+    char line[100];
+    printf("Put an terminal command here...\n");
+    read(STDIN_FILENO, line, 100);
+    char **args = parse_args(line);
+    execvp(args[0], args);
+}
